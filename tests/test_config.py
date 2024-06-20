e588bf3fe4d09a498fc09874e8f1c87094834679
christianbonnell <christian.bonnell@gmail.com>
2024-06-20 08:36:49 -0500

    Add tests for additional constructor args.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,22 +1,33 @@
 from pathlib import Path
+import pytest
 
 from curator.config import CuratorConfig
 
 
 config = CuratorConfig(Path.home() / ".curator")
+str_config = CuratorConfig("~/.curator")
 
 
 def test_root_directory():
     assert config.root_directory == Path.home() / ".curator"
+    assert str_config.root_directory == Path.home() / ".curator"
 
 
 def test_settings_file():
     assert config.settings_file == Path.home() / ".curator" / "curator.db"
+    assert str_config.settings_file == Path.home() / ".curator" / "curator.db"
 
 
 def test_remote_collections_file():
     assert config.remote_collections_file == Path.home() / ".curator" / "remote_collections.yaml"
+    assert str_config.remote_collections_file == Path.home() / ".curator" / "remote_collections.yaml"
 
 
 def test_collections_root():
     assert config.collections_root == Path.home() / ".curator" / "collections"
+    assert str_config.collections_root == Path.home() / ".curator" / "collections"
+
+
+def test_curator_config_no_root_directory():
+    with pytest.raises(ValueError):
+        CuratorConfig(None)
