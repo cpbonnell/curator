249958548c9b4d8bad1702caad45c9909558d07e
cpbonnell <christian.bonnell@gmail.com>
2024-06-20 09:55:01 -0400

    Add config object for injection (#5)
    * Define basic config class with settings file.
    
    * Add a property for the collections.yaml file.
    
    * Add property for local collections root.
    
    * Add basic tests for the CuratorConfig class.
    
    * Add function to ensure the config represents a valid installation.
    
    * Validator checks for all files before raising.
    
    * Test for the installation validator.

---
+++ curator/config.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from typing import Union
+
+Pathlike: Union = str | Path
+
+DEFAULT_CURATOR_ROOT_DIRECTORY = Path.home() / ".curator"
+
+
+class CuratorConfig:
+    """
+    Configuration for the Curator application.
+    """
+
+    def __init__(self, curator_root: Pathlike):
+        """
+        Create a new CuratorConfig object.
+
+        It is strongly recommended to use the default curator_root of the application, since curator does not
+        need to be installed more than once for a user. This option is included for testing purposes, or in
+        case the user wants to install Curator in a non-standard location.
+
+        :param curator_root:  The root directory for this installation of Curator.
+        """
+
+        if not curator_root:
+            raise ValueError("A root_directory is required to instantiate a CuratorConfig object.")
+
+        if isinstance(curator_root, str):
+            curator_root = Path(curator_root)
+
+        self._curator_root = curator_root.expanduser().resolve()
+
+    @property
+    def root_directory(self) -> Path:
+        """
+        The root directory for this installation of Curator.
+        """
+        return self._curator_root
+
+    @property
+    def settings_file(self) -> Path:
+        """
+        The database file where Curator stores its settings and other information.
+        """
+        return self._curator_root / "curator.db"
+
+    @property
+    def remote_collections_file(self) -> Path:
+        """
+        The file where information about remote collections is stored.
+        """
+        return self._curator_root / "remote_collections.yaml"
+
+    @property
+    def collections_root(self) -> Path:
+        """
+        The root directory where files for local collections are stored.
+        """
+        return self._curator_root / "collections"
+
+    def assert_valid_installation(self):
+        """
+        Ensure that the root directory and all files exist, raise error if it doesn't.
+        """
+        missing_files = list()
+        if not self._curator_root.exists():
+            missing_files.append(self._curator_root)
+
+        if not self.settings_file.exists():
+            missing_files.append(self.settings_file)
+
+        if not self.remote_collections_file.exists():
+            missing_files.append(self.remote_collections_file)
+
+        if not self.collections_root.exists():
+            missing_files.append(self.collections_root)
+
+        if missing_files:
+            raise FileNotFoundError(f"The following files or directories are missing: {missing_files}")
---
+++ tests/test_config.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
+
+from curator.config import CuratorConfig
+
+config = CuratorConfig(Path.home() / ".curator")
+str_config = CuratorConfig("~/.curator")
+
+
+def test_root_directory():
+    assert config.root_directory == Path.home() / ".curator"
+    assert str_config.root_directory == Path.home() / ".curator"
+
+
+def test_settings_file():
+    assert config.settings_file == Path.home() / ".curator" / "curator.db"
+    assert str_config.settings_file == Path.home() / ".curator" / "curator.db"
+
+
+def test_remote_collections_file():
+    assert config.remote_collections_file == Path.home() / ".curator" / "remote_collections.yaml"
+    assert str_config.remote_collections_file == Path.home() / ".curator" / "remote_collections.yaml"
+
+
+def test_collections_root():
+    assert config.collections_root == Path.home() / ".curator" / "collections"
+    assert str_config.collections_root == Path.home() / ".curator" / "collections"
+
+
+def test_curator_config_no_root_directory():
+    with pytest.raises(ValueError):
+        CuratorConfig(None)
+
+
+def test_assert_valid_installation():
+    """
+    The assert_valid_installation method raises FileNotFound errors on the
+    absence of a variety of files. To ensue that the method is working as
+    expected, this test creates an empty temp director, and then adds the
+    needed files one by one until the method no longer raises an error.
+
+    """
+
+    with TemporaryDirectory() as tmpdirname:
+        bad_config = CuratorConfig(tmpdirname)
+
+        # Empty root directory
+        with pytest.raises(FileNotFoundError):
+            bad_config.assert_valid_installation()
+
+        # Add settings file
+        (Path(tmpdirname) / "curator.db").touch()
+        with pytest.raises(FileNotFoundError):
+            bad_config.assert_valid_installation()
+
+        # Add remote collections file
+        (Path(tmpdirname) / "remote_collections.yaml").touch()
+        with pytest.raises(FileNotFoundError):
+            bad_config.assert_valid_installation()
+
+        # Add collections directory (function should now return without raising an error)
+        (Path(tmpdirname) / "collections").mkdir()
+        bad_config.assert_valid_installation()
