ea3b1f7b758444c519bcd08c5576494bc8fd4951
christianbonnell <christian.bonnell@gmail.com>
2024-07-01 13:08:29 -0500

    Formal tests for alembic migrations during installation.

--- tests/test_sub_commands.py
+++ tests/test_sub_commands.py
@@ -1,5 +1,7 @@
 from curator.config import CuratorConfig
 from tempfile import TemporaryDirectory
+from sqlalchemy import create_engine, text
+import pdb
 
 
 def test_main():
@@ -8,10 +10,25 @@ def test_main():
     with TemporaryDirectory() as temp_dir:
         config = CuratorConfig(curator_root=temp_dir)
         main(config)
+
+        # Check for the existence of the root directory and collections root
         assert config.root_directory.exists()
         assert config.collections_root.exists()
 
-        # The following lines are not yet implemented in the command.
-        # assert config.settings_file.exists()
-        # assert config.remote_collections_file.exists()
-        # assert config.settings_file_database_url == f"sqlite:///{config.settings_file.as_uri()}"
+        # Ensure the database file exists and has the correct URL
+        assert config.settings_file.as_posix() == f"{config.root_directory.as_posix()}/curator.db"
+        assert config.settings_file_database_url == f"sqlite:///{config.settings_file.as_posix()}"
+        assert config.settings_file.exists()
+
+        # Ensure the image_metadata table exists in the database
+        engine = create_engine(config.settings_file_database_url)
+        with engine.connect() as connection:
+            result = connection.execute(text("SELECT name FROM sqlite_master where type='table'"))
+            all_tables = [result[0] for result in result.fetchall()]
+
+            # Get a list of all alembic revisions applied to this database
+            result = connection.execute(text("SELECT * FROM alembic_version"))
+            alembic_revisions = [result[0] for result in result.fetchall()]
+
+        assert "image_metadata" in all_tables
+        assert len(alembic_revisions) > 0
