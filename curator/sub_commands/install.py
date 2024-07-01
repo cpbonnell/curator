ccc787c227c9b1d59faa8aa0b70c61e10a736d1a
christianbonnell <christian.bonnell@gmail.com>
2024-07-01 10:56:41 -0500

    Run Alembic migrations as part of installation.

--- curator/sub_commands/install.py
+++ curator/sub_commands/install.py
@@ -1,4 +1,5 @@
 from curator.config import CuratorConfig
+from curator.alembic.tools import run_migrations
 
 
 def main(config: CuratorConfig) -> None:
@@ -6,6 +7,9 @@ def main(config: CuratorConfig) -> None:
     Initialize all files needed for a distinct Curator installation.
     """
 
-    # Step 0: If the root of the installation does not exist, create it
+    # Step 0: If the root directories of the installation does not exist, create it
     config.root_directory.expanduser().mkdir(parents=True, exist_ok=True)
     config.collections_root.expanduser().mkdir(parents=True, exist_ok=True)
+
+    # Step 1: Create the database file
+    run_migrations(config.settings_file_database_url)
